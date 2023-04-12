001a16b439c69d6bc52de2afd87e30a935ec93df
seulki <seulki901234@gmail.com>
2023-04-12 21:37:35 +0900

    9:36

---
+++ 프론트단/js/classroom.js
@@ -0,0 +1,56 @@
+/* class1 */ 
+$(document).ready(function() {
+    $('#div1-1').click(function() {
+      $('#div1-1').hide();
+      $('#div1-2').show();
+    });
+    
+    $('#div1-2').click(function() {
+      $('#div1-2').hide();
+      $('#div1-1').show();
+    });
+  });
+
+  /* class2 */ 
+  $(document).ready(function() {
+    $('#div2-1').click(function() {
+      $('#div2-1').hide();
+      $('#div2-2').show();
+    });
+    
+    $('#div2-2').click(function() {
+      $('#div2-2').hide();
+      $('#div2-1').show();
+    });
+  });
+
+  /* class3 */ 
+
+  $(document).ready(function() {
+    $('#div3-1').click(function() {
+      $('#div3-1').hide();
+      $('#div3-2').show();
+    });
+    
+    $('#div3-2').click(function() {
+      $('#div3-2').hide();
+      $('#div3-1').show();
+    });
+  });
+
+  /* class4 */ 
+  $(document).ready(function() {
+    $('#div4-1').click(function() {
+      $('#div4-1').hide();
+      $('#div4-2').show();
+    });
+    
+    $('#div4-2').click(function() {
+      $('#div4-2').hide();
+      $('#div4-1').show();
+    });
+  });
+
+  
+
+
