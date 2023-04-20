173ee82e56580c27f89146e2499454984b577838
HYUNGJU\hyung <hyungju9753@gmail.com>
2023-04-20 11:35:13 +0900

    병합용커밋

--- 프론트단/js/myClass.js
+++ 프론트단/js/myClass.js
@@ -1,11 +1,46 @@
-const goClass = document.querySelector(".goClass");
-const index = document.querySelector(".classIndex");
+// const goClass =document.querySelector(".goClass").get();
+// const index = document.querySelector(".classIndex").get();
+
+// goClass.addEventListener("mouseover",changeColor);
+// goClass.addEventListener("mouseleave",changeColor);
 
 
-goClass.addEventListener("mouseover", function() {
+// function changeColor(e){
+//     for(var i =0; i<goClass.length;i++){
+//         if (e.type === 'mouseover'){
+//             index[i].style.color="rgb(255, 142, 29)";
+//         } else if(e.type ==='mouseleave'){
+//             index[i].style.color="white";
+//         }
+//     }
+// }
 
-    index.style.color = "#fe6936";
+// goClass.addEventListener("mouseover", function() {
+//     for(var i =0; i<goClass.length;i++){
+//     index[i].style.color = "rgb(255, 142, 29)";
+// }
+// })
+// goClass.addEventListener("mouseleave", function() {
+//     for(var i =0; i<goClass.length;i++){
+//     index[i].style.color = "white";
+//     }
+// })
+$(document).ready(function(){
+    var goClass =$('.goClass').get();
+    var index =$('.classIndex').get();
+    if(goClass.addEventListener("mouseover")){
+    for(var i = 0; i<goClass.length; i++){
+        
+            index[i].css("color","rgb(255, 142, 29)")
+       
+    }
+}
+
 })
-goClass.addEventListener("mouseleave", function() {
-    index.style.color = "#cecece";
-})
+
+
+    // $(".goClass").hover(function(){
+    //     $(".classIndex").css("color","rgb(255, 142, 29)")
+   
+    // })
+
